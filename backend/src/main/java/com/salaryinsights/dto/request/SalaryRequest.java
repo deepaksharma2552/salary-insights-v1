9e1cde6dcec935658e53d5cca3af9cca9af5700c
Deepak Sharma <deepak.sharma@okta.com>
2026-03-17 11:23:07 +0530

    Internal Level- Mandatory validation  added

--- backend/src/main/java/com/salaryinsights/dto/request/SalaryRequest.java
+++ backend/src/main/java/com/salaryinsights/dto/request/SalaryRequest.java
@@ -26,6 +26,7 @@ public class SalaryRequest {
     @NotNull
     private ExperienceLevel experienceLevel;
 
+    @NotNull
     private com.salaryinsights.enums.InternalLevel companyInternalLevel;
 
     @Size(max = 200)
